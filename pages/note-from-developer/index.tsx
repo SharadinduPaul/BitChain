60499c0eb3b6cbbef14e6976bce31952f2e7ad2a
SharadinduPaul <sharadindu.paul007@gmail.com>
2022-08-19 01:17:06 +0530

    submit #2

--- pages/note-from-developer/index.tsx
+++ pages/note-from-developer/index.tsx
@@ -11,11 +11,12 @@ const Note: NextPage = () => {
         <br />
         <br />
         I am Sharadindu Paul, a React/ Next/ React Native frontend developer.
-        This website was built by me as an assignment, but that's not it. I have
-        put a lot of time and effort making this website stand out.
+        This website was built by me as an assignment, but that&apos;s not it. I
+        have put a lot of time and effort making this website stand out.
         <br />
         So, I request you to send me your feedback (Contact details below). Even
-        if you didn't like it, please let me know why. I would love to improve.
+        if you didn&apos;t like it, please let me know why. I would love to
+        improve.
         <br />
         <br />
         Regards
@@ -36,8 +37,8 @@ const Note: NextPage = () => {
         <a
           href="https://celebrated-paprenjak-8be496.netlify.app/sharadindu-paul-resume.pdf"
           target="_blank"
+          rel="noreferrer"
         >
-          {" "}
           MyResume.pdf
         </a>
         <br />
